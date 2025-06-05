b22d9f03def18626cc1d1ff577f812b9abd727c9
coderninjai <pradipgajghate1@gmail.com>
2025-06-05 20:28:43 +0530

    Games

---
+++ Games.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int h[100],a[100];
+    for(int i=0;i<n;i++){
+        cin>>h[i]>>a[i];
+    }
+    int count=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i!=j&&h[i]==a[j]){
+                count++;
+            }
+        }
+    }
+    cout<<count;
+    return 0;
+}
