5f92fa833dd82e763b491bf35adf82bc4bde5052
coderninjai <pradipgajghate1@gmail.com>
2025-06-09 21:11:48 +0530

    Football

---
+++ Football.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    string k;
+    cin >> k;
+    for (int i = 0; i < k.size(); i++)
+    {
+        if (k[i] == k[i + 1])
+        {
+            count++;
+            if (count == 6)
+            {
+                cout << "YES";
+                break;
+            }
+        }
+    }
+    if (count < 6)
+    {
+        cout << "NO";
+    }
+    return 0;
+}
