aa5636f0b20736f9feb257a37df18408fc9158df
coderninjai <pradipgajghate1@gmail.com>
2025-07-23 22:04:49 +0530

    23/07/2025

---
+++ median.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector <int> a(3);
+    int n;
+    cin>>n;
+    while(n--){
+        for(int i=0;i<3;i++){
+            cin>>a[i];
+        }
+
+        sort(a.begin(),a.end());
+
+        cout<<a[1]<<endl;
+    }
+    return 0;
+}
